YazanKittaneh/who-owns-what

commit 4e81dcc7b801377aa58bfc0ef1664822489d896a

swap in new mapbox map style and upgrade package (#419)
Given that our access to our current MapBox styling broke this morning, this is a hotfix that swaps in a new mapbox style from our team MapBox account.

client/src/components/PropertiesMap.tsx
@@ -30,9 +30,9 @@ type State = {
 };
 
 const MAPBOX_ACCESS_TOKEN =
-  "pk.eyJ1IjoiZGFuLWthc3MiLCJhIjoiY2lsZTFxemtxMGVpdnVoa3BqcjI3d3Q1cCJ9.IESJdCy8fmykXbb626NVEw";
+  "pk.eyJ1IjoianVzdGZpeCIsImEiOiJja2hldmIxMmEwODVyMnNtZ2NkNGEyNjd0In0.4Piymx4ObHhSPq1K4MOZkw";
 
-const MAPBOX_STYLE = "mapbox://styles/dan-kass/cj657o2qu601z2rqbp1jgiys5";
+const MAPBOX_STYLE = "mapbox://styles/justfix/ckhevcljr02jg19l3jtw9h9w6";
 
 const Map = ReactMapboxGl({
   accessToken: MAPBOX_ACCESS_TOKEN,

commit 91f03583896a9dcee57e0e4fe8967a7e053914fb

upgraded packages and fixed typings (#420)
This PR fixes some outstanding typescript and package dependency issues introduced by the hotfix in #419.

client/src/components/PropertiesMap.tsx
@@ -10,7 +10,7 @@ import "styles/PropertiesMap.css";
 import "mapbox-gl/src/css/mapbox-gl.css";
 import { Trans, Select } from "@lingui/macro";
 import { AddressRecord } from "./APIDataTypes";
-import { Props as MapboxMapProps } from "react-mapbox-gl/lib/map";
+import { FitBounds, Props as MapboxMapProps } from "react-mapbox-gl/lib/map";
 import { Events as MapboxMapEvents } from "react-mapbox-gl/lib/map-events";
 import { withMachineInStateProps } from "state-machine";
 
@@ -24,7 +24,7 @@ type State = {
   hasWebGLContext: boolean;
   mapRef: any | null;
   mobileLegendSlide: boolean;
-  addrsBounds: number[][];
+  addrsBounds: FitBounds;
   addrsPoints: JSX.Element[];
   mapProps: MapboxMapProps & MapboxMapEvents;
 };
@@ -98,7 +98,7 @@ export default class PropertiesMap extends Component<Props, State> {
       hasWebGLContext: MapHelpers.hasWebGLContext(),
       mapRef: null,
       mobileLegendSlide: false,
-      addrsBounds: [[]], // bounds are represented as a 2d array of lnglats
+      addrsBounds: DEFAULT_FIT_BOUNDS, // bounds are represented as a 2d array of lnglats
       addrsPoints: [], // array of Features
       mapProps: {
         onStyleLoad: (map, _) => this.setState({ mapLoading: false, mapRef: map }),
@@ -184,10 +184,10 @@ export default class PropertiesMap extends Component<Props, State> {
 
       // build a bounding box around our new detail addr
       const newBounds = !!(lat && lng)
-        ? [
+        ? ([
             [lng - DETAIL_OFFSET, lat - DETAIL_OFFSET],
             [lng + DETAIL_OFFSET, lat + DETAIL_OFFSET],
-          ]
+          ] as FitBounds)
         : this.state.mapProps.fitBounds;
       this.setState({
         mapProps: {